f71ebbf51785a2bd3c93cbda5f1b9c1ed0965387
robjo82 <rjrdutg@gmail.com>
2020-06-12 11:45:30 +0200

    création des pages contact et histoire

--- easysport/easysport/views.py
+++ easysport/easysport/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def histoire(request):
+    return render(request, 'histoire.html')
